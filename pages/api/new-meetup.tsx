437f29f17fa66a8b016d3914795dba1d1aca3c91
Hannes Draba <H.Draba@porta.de>
2022-11-30 12:11:22 +0100

    error implementing mongodb

---
+++ pages/api/new-meetup.tsx
@@ -0,0 +1,26 @@
+import { MongoClient } from 'mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { username, password } from '../../private/keys';
+
+// /api/new-meetup
+// POST --> /api/new-meetup
+
+export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const data = req.body;
+    console.log('hey');
+    
+    const client = await MongoClient.connect(
+      `mongodb+srv://${username}:${password}@finaltrycluster.dcie8yz.mongodb.net/meetups?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'meetup inserted' });
+  }
+};
--- pages/new-meetup.tsx
+++ pages/new-meetup.tsx
@@ -1,19 +1,30 @@
 // domain.com/new-meetup
 
-import { Meetup } from "../components/meetups/MeetupItem";
-import NewMeetupForm from "../components/meetups/NewMeetupForm"
+import { Meetup } from '../components/meetups/MeetupItem';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetup = () => {
-    const addMeetupHandler = (enteredMeetupData: Meetup) => {
-    console.log(enteredMeetupData);
+  const addMeetupHandler = async (enteredMeetupData: Meetup) => {
+    console.log('meh1');
     
-    }
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json()
+    console.log('returned data: ', data);
+    
+  };
 
   return (
     <>
-    <NewMeetupForm onAddMeetup={addMeetupHandler} />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </>
-  )
-}
+  );
+};
 
-export default NewMeetup
+export default NewMeetup;
