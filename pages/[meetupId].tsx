3dc0b9ebd6c9761983de3f930ad14b1fa633aceb
Hannes Draba <H.Draba@porta.de>
2022-11-30 10:15:30 +0100

    added serverside rendering with staticeside

--- pages/[meetupId].tsx
+++ pages/[meetupId].tsx
@@ -1,16 +1,62 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
+import { useRouter } from 'next/router';
 import MeetupDetail from '../components/meetups/MeetupDetail';
+import { Meetup } from '../components/meetups/MeetupItem';
 
-const MeetupDetails = () => {
+type MeetupDetailsProps = {
+  meetupData: Meetup & { description: string };
+};
+
+const MeetupDetails = (props: MeetupDetailsProps) => {
   return (
     <>
       <MeetupDetail
         imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg?20130611211153"
         title="some first meetup"
-        address="some street 4, 55555 city"
-        description="description, yeah descriptive"
+        address="some sddress"
+        description="desc"
       />
     </>
   );
 };
+
+// why?: every variation of this site has to be prerendered, so we have to provide every variation via the paths array
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+// have to use getStaticPaths as well when using getStaticProps and a dynamic page
+export async function getStaticProps<GetStaticProps>(
+  context: GetStaticPropsContext
+) {
+  const meetupId = context.params!.meetupId;
+  
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg?20130611211153',
+        id: { meetupId },
+        title: 'some first meetup',
+        address: 'some street, 55555 city',
+        description: 'descriptive',
+      },
+    },
+  };
+}
 
 export default MeetupDetails;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,37 +4,55 @@ import { Meetup } from '../components/meetups/MeetupItem';
 import MeetupList from '../components/meetups/MeetupList';
 
 type HomePageProps = {
-    meetups: Meetup[]
-}
-const DUMMY_MEETUPS: Meetup[] = [{
+  meetups: Meetup[];
+};
+const DUMMY_MEETUPS: Meetup[] = [
+  {
     id: 'm1',
     title: 'meetup1',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg?20130611211153',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg?20130611211153',
     address: 'here',
     // description: 'some desc'
-}, {
+  },
+  {
     id: 'm2',
     title: 'meetup2',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg?20130611211153',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg?20130611211153',
     address: 'also here',
     // description: 'some desc'
-}]
+  },
+];
 
 const HomePage = (props: HomePageProps) => {
-  
-    return (
+  return (
     <>
-      <MeetupList meetups={props.meetups}/>
+      <MeetupList meetups={props.meetups} />
     </>
   );
 };
 
+// runs on the server
+// runs for every incoming request
+// export async function getServerSideProps (context) {
+    // const req = context.req
+    // const res = context.res
+    // 
+    // return {
+        // props: {
+            // meetups: DUMMY_MEETUPS
+        // }
+    // }
+// }
+
 // can executed code that normally runs on the server
 // executed during the build process, so it is never on the client-side
 export async function getStaticProps() {
-    return {
-        props: {meetups: DUMMY_MEETUPS}
-    }
+  return {
+    props: { meetups: DUMMY_MEETUPS },
+    revalidate: 10, // rerenders every x seconds when incoming requests are happening
+  };
 }
 
 export default HomePage;
